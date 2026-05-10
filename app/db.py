1ca980dd11250dab81738720109d37c7190706ea
saivarun1103 <varun.11n03@gmail.com>
2026-05-10 23:46:48 +0530

    Fix db pooling

--- app/db.py
+++ app/db.py
@@ -5,8 +5,8 @@ from app.config import DATABASE_URL
 
 engine = create_engine(
     DATABASE_URL,
-    pool_size=5,
-    max_overflow=5,
+    pool_size=10,
+    max_overflow=20,
     pool_timeout=30,
     pool_recycle=1800,
     pool_pre_ping=True
